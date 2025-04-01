78373bfc17be61db4c2c56ef723cfbed551d06b9
Yash Shirsath (GPU) <shirsath.yash+gpu@gmail.com>
2025-04-01 19:40:49 +0000

    split data into train and val

--- steering/data/fineweb-edu/prepare.py
+++ steering/data/fineweb-edu/prepare.py
@@ -43,26 +43,45 @@ if __name__ == "__main__":
     )
 
     # concatenate all the ids in each dataset into one large file we can use for training
-    arr_len = np.sum(tokenized["len"], dtype=np.uint64)
-    filename = os.path.join(os.path.dirname(__file__), f"train.bin")
+    total_len = np.sum(tokenized["len"], dtype=np.uint64)
     dtype = np.uint16  # (can do since enc.max_token_value == 50256 is < 2**16)
-    arr = np.memmap(filename, dtype=dtype, mode="w+", shape=(arr_len,))
+    train_frac = 0.9
+    train_len = int(total_len * train_frac)
+    val_len = total_len - train_len
+
+    train_name = os.path.join(os.path.dirname(__file__), f"train.bin")
+    val_name = os.path.join(os.path.dirname(__file__), f"val.bin")
+    train_arr = np.memmap(train_name, dtype=dtype, mode="w+", shape=(train_len,))
+    val_arr = np.memmap(val_name, dtype=dtype, mode="w+", shape=(val_len,))
     total_batches = 1024
 
-    idx = 0
-    for batch_idx in tqdm(range(total_batches), desc=f"writing {filename}"):
-        # Batch together samples for faster write
+    train_idx = 0
+    val_idx = 0
+    for batch_idx in tqdm(range(total_batches), desc=f"writing {train_name}"):
         batch = tokenized.shard(
             num_shards=total_batches, index=batch_idx, contiguous=True
         ).with_format("numpy")
         arr_batch = np.concatenate(batch["ids"])
-        # Write into mmap
-        arr[idx : idx + len(arr_batch)] = arr_batch
-        idx += len(arr_batch)
-    arr.flush()
+
+        # Calculate how many tokens go to train vs val
+        remaining_train = train_len - train_idx
+        if remaining_train > 0:
+            # Write to train array if we have space
+            train_size = min(len(arr_batch), remaining_train)
+            train_arr[train_idx : train_idx + train_size] = arr_batch[:train_size]
+            train_idx += train_size
+
+        else:
+            # If train array is full, write everything to val array
+            val_arr[val_idx : val_idx + len(arr_batch)] = arr_batch
+            val_idx += len(arr_batch)
+
+    train_arr.flush()
+    val_arr.flush()
 
     # train.bin is ~19GB
-    # train has ~10B tokens
+    print(f"train.bin has {train_idx:.2e} tokens")
+    print(f"val.bin has {val_idx:.2e} tokens")
 
     # to read the bin files later, e.g. with numpy:
     # m = np.memmap('train.bin', dtype=np.uint16, mode='r')
