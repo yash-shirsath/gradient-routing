81db325215ee75d8bd27904cc833adfb874cb48a
Yash S <shirsath.yash@gmail.com>
2025-04-22 13:40:08 -0400

    gpu initialization script

---
+++ start_gpu.sh
@@ -0,0 +1,29 @@
+# Load environment variables
+source .env
+
+git clone https://${GITHUB_TOKEN}@github.com/yash-shirsath/gradient-routing.git
+git config --global user.email "yash.shirsath@gmail.com"
+git config --global user.name "Yash Shirsath (GPU)"
+
+
+touch ~/.no_auto_tmux
+pip uninstall torch
+pip3 install --pre torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
+
+# in parallel
+sudo apt install build-essential
+
+code --install-extension ms-python.python
+code --install-extension nvidia.nsight-vscode-edition
+
+
+pip install transformers datasets jaxtyping wandb
+pip install -e .
+cd steering/ 
+
+wandb login ${WANDB_API_KEY}
+
+#gcloud 
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-linux-x86_64.tar.gz
+./google-cloud-sdk/install.sh
