89d7fac657402f885a0dd1f0c6bce3e60f9b36fa
Yash Shirsath (GPU) <yash.shirsath@gmail.com>
2025-04-24 22:29:56 +0000

    temporary changes to investigate if not enough high quality tokens surround target token

--- steering/config/gpt2.py
+++ steering/config/gpt2.py
@@ -8,27 +8,27 @@ always_save_checkpoint = False
 
 wandb_log = True
 wandb_project = "gradient-routing-fwe"
-wandb_run_name = "13-14-15"
+wandb_run_name = "6-18"
 
 dataset = "fineweb-edu"
 gradient_accumulation_steps = 40
-batch_size = 32
-block_size = 1024
+batch_size = 64
+block_size = 512
 
 # model config
-n_layer = 24
-n_head = 16
-n_embd = 1024
+n_layer = 10
+n_head = 8
+n_embd = 512
 dropout = 0.2
 
-learning_rate = 6e-4
+learning_rate = 1e-3
 max_iters = 5000
 lr_decay_iters = 5000
-min_lr = 6e-5  # learning_rate / 10 usually
+min_lr = 1e-4  # learning_rate / 10 usually
 beta1 = 0.9
 beta2 = 0.95
 
-warmup_iters = 1000
+warmup_iters = 500
 
 
 device = "cuda"
@@ -37,8 +37,8 @@ compile = False
 
 # Gradient Routing Config Values
 target_words = {
-    "python": 0,
+    "California": 0,
 }
 
-target_layers = set([13, 14, 15])
+target_layers = set([6, 7, 8, 9, 10, 11, 12, 13])
 top_tokens_log_frequently = True
